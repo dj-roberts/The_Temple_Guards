f1af85c47e8ac24e55f25ad35c0f583fe22168f6
Nick2046L <68716022Nick@users.noreply.github.com>
2020-10-30 11:02:36 +1100

    sunset demo
    sunset demo

---
+++ Liminal_VR_Temple_Guards/Library/PackageCache/com.liminal.sdk@f27c9e5b364bcb74daf7506736cfc05ecfe15e7c/SDK/Systems/Reflection System/Scripts/EDeviceModelType.cs
@@ -0,0 +1,15 @@
+﻿namespace Liminal.Systems
+{
+    public enum EDeviceModelType
+    {
+        Go,
+        Quest,
+        AcerAH101,
+        HtcVive,
+        HtcVivePro,
+        HtcViveCosmos,
+        RiftS,
+        Rift,
+        Unknown
+    }
+}
